f2608ea8f7d6446abf5b3a123d04691e3667017b
JyothikSandeep <101253647+JyothikSandeep@users.noreply.github.com>
2023-06-24 04:04:06 +0530

    creation of files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("api is running sucesfully");
 });
 
-app.get()
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
-app.listen(5000, console.log("server is running on PORT 5000"));
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log("server is running on PORT 5000"));
